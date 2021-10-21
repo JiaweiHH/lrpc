be890f37b81054856942558dbfa981e7301adb1a
jiawei <lixpoo@163.com>
2021-10-21 17:00:11 +0800

    s03 线程间任务迁移和多线程 EventLoop

---
+++ tests/test6.cc
@@ -0,0 +1,26 @@
+#include "../EventLoop.h"
+#include "../EventLoopThread.h"
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std::chrono_literals;
+
+/// 测试跨线程调用 runInLoop 和 runAfter
+
+void runInThread() {
+  auto tid = std::this_thread::get_id();
+  printf("runInThread(): pid = %d, tid = 0x%lx\n", getpid(), *(uint64_t *)&tid);
+}
+
+int main() {
+  auto tid = std::this_thread::get_id();
+  printf("main(): pid = %d, tid = 0x%lx\n", getpid(), *(uint64_t *)&tid);
+
+  imitate_muduo::EventLoopThread loopThread;
+  imitate_muduo::EventLoop *loop = loopThread.startLoop();
+  loop->runInLoop(runInThread);
+  std::this_thread::sleep_for(1s);
+  loop->runAfter(2, runInThread);
+  loop->quit();
+  printf("exit main().\n");
+}
